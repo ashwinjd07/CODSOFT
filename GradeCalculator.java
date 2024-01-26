eb2cf2bdd5bfe2136d9cbff6ea60a153b97190a9
ashwinjd07 <119476490+ashwinjd07@users.noreply.github.com>
2024-01-26 13:03:26 +0530

    Add files via upload

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -4,7 +4,6 @@ public class GradeCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input: Take marks obtained (out of 100) in each subject.
         System.out.print("Enter the number of subjects: ");
         int subjects = scanner.nextInt();
 
@@ -15,19 +14,16 @@ public class GradeCalculator {
             marks[i] = scanner.nextInt();
         }
 
-        // Calculate Total Marks: Sum up the marks obtained in all subjects.
         int totalMarks = 0;
         for (int mark : marks) {
             totalMarks += mark;
         }
-
-        // Calculate Average Percentage: Divide the total marks by the total number of subjects to get the average percentage.
         double averagePercentage = (double) totalMarks / subjects;
 
-        // Grade Calculation: Assign grades based on the average percentage achieved.
+      
         char grade = calculateGrade(averagePercentage);
 
-        // Display Results: Show the total marks, average percentage, and the corresponding grade to the user.
+       
         System.out.println("\nTotal Marks: " + totalMarks);
         System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
         System.out.println("Grade: " + grade);
